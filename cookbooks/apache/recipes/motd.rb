831634efc84c79684de1b7788335a4d58fba4c0d
Shashank Sharma <shankyup43@hotmail.com>
2018-01-22 14:18:23 +0000

    Added web role

--- cookbooks/apache/recipes/motd.rb
+++ cookbooks/apache/recipes/motd.rb
@@ -1,7 +1,7 @@
 #This file will update the MOTD file on node
 
 hostname = node['hostname']
-
+ip = node['ipaddress']
 file '/etc/motd' do
-	content 'The name of this node is: ' #{hostname}
+	content "The name of this node is:  #{hostname} and the IP address is: #{ip}"
 end
